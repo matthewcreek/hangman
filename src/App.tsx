e35dabb74af22f7765f47b117c283cddd986359a
Matthew Creek <matthew.creek27@gmail.com>
2023-08-22 16:01:46 -0500

    established functionality for enter key presses to give a new word

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import HangmanDrawing from "./HangmanDrawing"
 import HangmanWord from "./HangmanWord"
 import Keyboard from "./Keyboard"
 
-
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+  const [wordToGuess, setWordToGuess] = useState(getWord)
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 
@@ -38,6 +38,24 @@ function App() {
       document.removeEventListener('keypress', handler)
     }
   },[addGuessedLetter])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+
+      if (key !== 'Enter') return;
+
+      e.preventDefault()
+      setGuessedLetters([])
+      setWordToGuess(getWord())
+    }
+
+    document.addEventListener('keypress', handler)
+
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [])
 
   console.log(wordToGuess)
   return (
